c6140dc566b56978f46aaecd8cf68d3528b360f4
Defne-A <daydin@arizona.edu>
2025-02-24 20:55:17 -0700

    Update Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -6,6 +6,23 @@ public class Playlist{
     this.name = title;
   }
 
+  public void addSong(Song song){
+    list.add(song);
+  }
+
+  public ArrayList<Song> getPlaylist(){
+    ArrayList<Song> playlist = new ArrayList<Song>();
+    for (Song s: list){
+      playlist.add(s);
+    }
+    return playlist;
+  }
+
+  public String getTitle(){
+    String title = name;
+    return title;
+  }
+
   public void changeName(String name){
     this.name = name;
   }
