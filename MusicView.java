7a68450be4af3af4148f164a0f89212eaa5a1e9b
voidzer00 <86978266+voidzer00@users.noreply.github.com>
2025-02-27 04:10:45 -0700

    Update MusicView.java
    added basic functionality for the view

--- MusicView.java
+++ MusicView.java
@@ -1,14 +1,6 @@
-public class MusicView {
-
-  public static void main(String[] args) {
-
-
-  }
-
-
-
-
-
-
-
+import java.io.IOException;
+public class MusicView{
+    public static void main(String[] args){
+    LibraryModel model = new LibraryModel();           
+    }
 }
