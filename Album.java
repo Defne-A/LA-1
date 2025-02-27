0868b49115f53b4e18a7a511471e983b02a0be72
voidzer00 <86978266+voidzer00@users.noreply.github.com>
2025-02-27 04:07:48 -0700

    Update Album.java
    Fixed NULLPOINTEREXCEPTION from this class. Added some toString() functionality.

--- Album.java
+++ Album.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 
 public class Album {
-  private ArrayList<Song> tracklist;
+  private ArrayList<Song> tracklist = new ArrayList<Song>(); //this was causing a null exception, so i changed it
   private String artist;
   private String genre;
   private String title;
-
-  public Album(String artist, String genre, String title) {
+  private String year;
+  public Album(String artist, String genre, String title,String year) {
     this.artist = artist;
     this.genre = genre;
     this.title = title;
+    this.year = year;
   }
 
-  public void addSong(Song song){
+  public void addSong(Song song){ //make sure to avoid the escaping reference to song here
     this.tracklist.add(song);
   }
 
@@ -20,15 +21,25 @@ public class Album {
     String name = artist;
     return name;
   }
-
   public String getGenre(){
     String gen = genre;
     return gen;
   }
 
   public String getAlbumTitle(){
-    String alb = title;
+    String alb = title; //is this required? Strings are immutable so idk if this matters
     return alb;
+  }
+  public String getYear(){
+      return this.year;
+  }
+  @Override
+  public String toString(){
+    String netStr = "";
+    for(Song s : this.tracklist){
+        netStr+= s.toString() + "\n";
+    }
+    return netStr;
   }
   
 }
