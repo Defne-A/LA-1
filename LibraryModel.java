02534100443a9c9a2a0049466c6e397d7479fdf8
voidzer00 <86978266+voidzer00@users.noreply.github.com>
2025-02-27 04:09:56 -0700

    Update LibraryModel.java
    added basic functionality

--- LibraryModel.java
+++ LibraryModel.java
@@ -1,12 +1,14 @@
-public class LibraryModel {
-  
-
-
-
-
-
-
-
-
-
+import java.io.IOException;
+public class LibraryModel{
+    public LibraryModel(){
+        //interacts with the view, playlist, ect.
+        try{
+        MusicStore m1 = new MusicStore();
+        m1.readFile();
+        m1.printLibrary();
+    }
+    catch(IOException e){
+        System.out.println("TEST");
+    }
+    }
 }
