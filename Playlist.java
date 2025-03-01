be6c066a8eb2c66ec89c9991cfac440b1350c957
voidzer00 <86978266+voidzer00@users.noreply.github.com>
2025-02-28 22:49:00 -0700

    Update Playlist.java
    added package name

--- Playlist.java
+++ Playlist.java
@@ -1,30 +1,32 @@
-public class Playlist{
-  private ArrayList<Song> list;
-  private String name;
+package la1;
+import java.util.ArrayList;
+public class Playlist {
+    private ArrayList<Song> list;
+    private String name;
 
-  public Playlist(String title){
-    this.name = title;
-  }
-
-  public void addSong(Song song){
-    list.add(song);
-  }
-
-  public ArrayList<Song> getPlaylist(){
-    ArrayList<Song> playlist = new ArrayList<Song>();
-    for (Song s: list){
-      playlist.add(s);
+    public Playlist(String title) {
+        this.name = title;
+        this.list = new ArrayList<Song>(); // Initialize the list to prevent NullPointerException
     }
-    return playlist;
-  }
 
-  public String getTitle(){
-    String title = name;
-    return title;
-  }
+    public void addSong(Song song) {
+        list.add(song);
+    }
 
-  public void changeName(String name){
-    this.name = name;
-  }
+    public ArrayList<Song> getPlaylist() {
+        ArrayList<Song> playlist = new ArrayList<Song>();
+        for (Song s: list) {
+            playlist.add(s);
+        }
+        return playlist;
+    }
 
+    public String getTitle() {
+        String title = name;
+        return title;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
